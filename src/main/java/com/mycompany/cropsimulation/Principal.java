e4a307148e334a8de74c9ea266426ddd8669679e
sofia <sofia@DESKTOP-7HGAQ6R>
2021-05-10 17:48:47 -0500

    Prueba

--- src/main/java/com/mycompany/cropsimulation/Principal.java
+++ src/main/java/com/mycompany/cropsimulation/Principal.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
 https://github.com/sofiacarrilloek/cropSimulation.git
+probando
  */
 package com.mycompany.cropsimulation;
 
@@ -17,6 +18,7 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba");
     }
     
 }
